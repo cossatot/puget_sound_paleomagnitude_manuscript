99b4364346d0ac1e006d4ddd74175ee603a68bb3
itchy <richard.h.styron@gmail.com>
2017-08-10 21:00:32 -0700

    script ready to go for review

--- scripts/p_M_DL.py
+++ scripts/p_M_DL.py
@@ -14,6 +14,10 @@ from culpable.stats import Pdf
 import seaborn as sns
 #sns.set_palette('dark')
 
+
+save_results = True
+
+
 np.random.seed(69)
 
 n_iters = 1000  # number of monte carlo iterations
@@ -25,7 +29,7 @@ with open('../gis/puget_lowland_ruptures.geojson', 'r') as f:
 
 rs = r['features']
 
-eq_names = list(set(rr['properties']['event'] for rr in rs))
+eq_names = sorted(list(set(rr['properties']['event'] for rr in rs)))
 
 eq_df = pd.DataFrame(index=eq_names, 
                      columns=['fault', 'offset', 'offset_err',
@@ -103,7 +107,9 @@ M_max = 8.5
 M_step = 0.01
 
 Ms = np.arange(M_min, M_max+M_step, M_step)
-p_M = Pdf(Ms, np.ones(len(Ms)))
+p_M = cp.magnitudes.make_p_M(p_M_type='uniform',
+                             #p_M_type='GR_surface_break', 
+                             p_M_min=M_min, p_M_max=M_max, M_step=M_step)
 
 # do inversion
 p_M_D_dict = OrderedDict()
@@ -152,8 +158,8 @@ for i, (eq, pmdl) in enumerate(p_M_DL_dict.items()):
                  yerr=y_err,
                  lw=0.75,
                  fmt='.',
-
                  )
+
     # saving quantiles for analysis
     res_df.loc[eq] = (cp.stats.pdf_mean(pmd.x, pmd.y),
                       p50d, p25d, p75d,
@@ -162,13 +168,13 @@ for i, (eq, pmdl) in enumerate(p_M_DL_dict.items()):
 
 
 ax00.set_xlim([M_min, M_max])
-ax1.set_xlim([6.0, M_max])
+ax1.set_xlim([M_min, M_max])
 
-ax01.set_ylim([0,6])
+ax01.set_ylim([0,5.5])
 
 ax01.set_xlabel('Magnitude M')
-ax00.set_ylabel('p(M|D)')
-ax01.set_ylabel('p(M|D,L)')
+ax00.set_ylabel('Probability p(M|D)')
+ax01.set_ylabel('Probability p(M|D,L)')
 ax00.text(5.51, 1.45, 'a', fontsize=12, weight='bold')
 ax01.text(5.51, 5.50, 'b', fontsize=12, weight='bold')
 
@@ -180,15 +186,17 @@ f1.tight_layout()
 f0.tight_layout(h_pad=0)
 #f0.subplots_adjust(hspace=0)
 
-
-f0.savefig('../figures/posterior_pdfs.pdf')
-f1.savefig('../figures/posterior_scatter.pdf')
+if save_results:
+    f0.savefig('../figures/posterior_pdfs.pdf')
+    f1.savefig('../figures/posterior_scatter.pdf')
 
 # Save results to DF
 eq_df['M_mean'] = res_df.pmdl_mean.values
 eq_df = eq_df.sort_values('M_mean', ascending=False)
 eq_df['eq_name'] = list(eq_df.index)
-eq_df.to_csv('../results/eq_table.csv', index=False)
+
+if save_results:
+    eq_df.to_csv('../results/eq_table.csv', index=False)
 
 
 
@@ -224,7 +232,8 @@ plt.ylabel('Rupture Offset (m)')
 plt.legend(loc='lower right')
 f2.subplots_adjust(bottom=0.12)
 
-f2.savefig('../figures/l_d_scaling.pdf')
+if save_results:
+    f2.savefig('../figures/l_d_scaling.pdf')
 
 
 # Magnitude PDFs for each earthquake
@@ -263,9 +272,23 @@ axs[-1].set_xlabel('Moment Magnitude')
 
 f3.subplots_adjust(hspace=0, left=0.02, right=0.6, top=0.98, bottom=0.05)
 
-f3.savefig('../figures/post_magnitude_stack.pdf')
+if save_results:
+    f3.savefig('../figures/post_magnitude_stack.pdf')
+
+
+# results to json
+res_dict = {}
+for i, (eq, pmdl) in enumerate(p_M_DL_dict.items()):
+    
+    pmd = list(p_M_D_dict.items())[i][1]
+    
+    res_dict[eq] = {'M': pmdl.x.tolist(),
+                    'p_M_D': pmd.y.tolist(),
+                    'p_M_DL': pmdl.y.tolist()}
+
+if save_results:
+    with open('../results/magnitude_posteriors.json', 'w') as f:
+        json.dump(res_dict, f)
+
 
 plt.show()
-
-
-
